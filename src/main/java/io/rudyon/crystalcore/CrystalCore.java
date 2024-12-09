25ddfd1122732602a3c8306ec8008fcedcd2897c
rudyon <keremberket@gmail.com>
2024-12-09 18:52:24 +0300

    biopolymer and steel

--- src/main/java/io/rudyon/crystalcore/CrystalCore.java
+++ src/main/java/io/rudyon/crystalcore/CrystalCore.java
@@ -1,19 +1,23 @@
 package io.rudyon.crystalcore;
 
+import net.fabricmc.api.ModInitializer;
+import org.quiltmc.loader.api.QuiltLoader;
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.loader.api.ModMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CrystalCore implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "crystalcore";
 	public static final Logger LOGGER = LoggerFactory.getLogger("CrystalCore");
 
 	@Override
-	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+	public void onInitialize() {
+		// Get the mod container and metadata using QuiltLoader
+		ModContainer mod = QuiltLoader.getModContainer("crystalcore").orElseThrow();
+		ModMetadata metadata = mod.metadata();
+
+		LOGGER.info("Hello Quilt world from {}!", metadata.name());
 		CrystalCoreItems.register(mod);
 	}
 }
--- src/main/java/io/rudyon/crystalcore/CrystalCoreItems.java
+++ src/main/java/io/rudyon/crystalcore/CrystalCoreItems.java
@@ -1,25 +1,40 @@
 package io.rudyon.crystalcore;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.Items;
+import net.minecraft.client.toast.SystemToast;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 
-import java.util.function.Function;
-
 public class CrystalCoreItems {
+	public static final RegistryKey<ItemGroup> CRYSTAL_CORE_TAB = RegistryKey.of(Registries.ITEM_GROUP.getKey(), new Identifier(CrystalCore.MOD_ID, "item_group"));
+
+	public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+		.icon(() -> new ItemStack(CrystalCoreItems.FLUXIUM))  // Set the icon for the tab
+		.name(Text.translatable("itemGroup." + CrystalCore.MOD_ID + ".crystal_core_tab"))  // Set the display name for the item group
+		.build();
+
 	public static final Item FLUXIUM = new Item(new Item.Settings());
+	public static final Item BIOPOLYMER = new Item(new Item.Settings());
+	public static final Item STEEL = new Item(new Item.Settings());
+
 
 	public static void register(ModContainer mod) {
-		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "fluxium"), FLUXIUM);
+		Registry.register(Registries.ITEM_GROUP, CRYSTAL_CORE_TAB, CUSTOM_ITEM_GROUP);
 
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "fluxium"), FLUXIUM);
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "biopolymer"), BIOPOLYMER);
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "steel"), STEEL);
+
+		ItemGroupEvents.modifyEntriesEvent(CRYSTAL_CORE_TAB).register(entries -> {
 			entries.addItem(FLUXIUM);
+			entries.addItem(BIOPOLYMER);
+			entries.addItem(STEEL);
 		});
 	}
 }
